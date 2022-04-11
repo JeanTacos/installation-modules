4588bcbf482a49c438859abb76dc38505dafec0a
JeanTacos <103442066+JeanTacos@users.noreply.github.com>
2022-04-11 17:36:49 +0200

    Update php7.4.sh

--- php7.4.sh
+++ php7.4.sh
@@ -1,1 +1,10 @@
+sudo apt -y install lsb-release apt-transport-https ca-certificates 
+sudo wget -O /etc/apt/trusted.gpg.d/php.gpg https://packages.sury.org/php/apt.gpg
 
+echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" | sudo tee /etc/apt/sources.list.d/php.list
+
+sudo apt update
+
+sudo apt -y install php7.4
+
+apt -y install php7.4 php7.4-{cli,gd,mysql,pdo,mbstring,tokenizer,bcmath,xml,fpm,curl,zip}
